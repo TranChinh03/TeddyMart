c92c75266f17d8a7df73e4266e2cb930dd53592f
thuong22092k3 <everything22092k3@gmail.com>
2023-10-07 14:18:09 +0700

    Set global Color - Font Size

--- teddymart/tailwind.config.js
+++ teddymart/tailwind.config.js
@@ -6,6 +6,35 @@ module.exports = {
       spacing: {
         0.1: "0.1rem",
       },
+      colors: {
+        main_bg: "#f5f5f5",
+        main_form: "#ffffff",
+
+        btn_main_bg: "#e6a247",
+        btn_second_bg: "#211f30",
+
+        sidebar: "#207ca3",
+        highlight_sidebar: "#64a4bf",
+        hover: "#cee8f5",
+
+        txt_main_color: "#000000de",
+        txt_mediumgrey: "#888",
+        txt_lightgrey: "#9a9a9a",
+        txt_white: "#fff",
+
+        message_success: "#51a351",
+        checkbox_bg: "#f34334",
+      },
+
+      fontSize: {
+        12: "12px",
+        13: "13px",
+        14: "14px",
+        17: "17px",
+        19: "19px",
+        20: "20px",
+        2.2:"2.2rem",
+      },
     },
   },
   plugins: [],
