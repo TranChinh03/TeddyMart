770c06053bbc1b22f0a2a6933bd1674eb2eefcb1
TranChinh03 <21520166@gm.uit.edu.vn>
2023-10-09 01:14:56 +0700

    Init Firebase

---
+++ teddymart/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBdTF21koiqn_pGkKbCCXTXhFmII4ldFn0",
+  authDomain: "teddymart-90d05.firebaseapp.com",
+  projectId: "teddymart-90d05",
+  storageBucket: "teddymart-90d05.appspot.com",
+  messagingSenderId: "697095652159",
+  appId: "1:697095652159:web:e7578783464d6edda4498a",
+  measurementId: "G-NLNSYCY89X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
