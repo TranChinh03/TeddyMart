450267f34b96739003ccc1ac17ed0548dcdc2a5a
Tien25012003 <21520112@gm.uit.edu.vn>
2023-11-01 08:28:56 +0700

    Fix Nav

--- teddymart/src/App.js
+++ teddymart/src/App.js
@@ -2,7 +2,7 @@ import RootRoutes from "./routes/route/RootRoutes";
 import LoginScreen from "views/Authentication/LoginScreen";
 import SignUpScreen from "views/Authentication/SignUpScreen";
 const App = () => {
-  return <SignUpScreen/>;
+  return <RootRoutes />;
 };
 
 export default App;
