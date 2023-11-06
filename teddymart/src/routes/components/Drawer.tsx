ebc57d31048005b690973a84692d784f5506f99f
helloworld32144 <nguyenthilinhchi205@gmail.com>
2023-11-07 01:04:33 +0700

    Drawer

--- teddymart/src/routes/components/Drawer.tsx
+++ teddymart/src/routes/components/Drawer.tsx
@@ -39,46 +39,23 @@ const DRAWER_ITEM = [
 ];
 export default function Drawer() {
   const navigate = useNavigate();
+  const [currentTab, setCurrentTab] = useState("/");
   return (
-    <div
-      className="h-screen w-full top-0 left-0 shadow-lg"
-      style={{ backgroundColor: "#217ca3" }}
-    >
-      <div className="overflow-y-auto h-full">
-        {/* Shop Header */}
-        <div className="flex items-center justify-start p-5">
-          <div className="flex w-16 h-16 items-center justify-center bg-gray-500 rounded-full text-white text-3xl font-semibold">
-            S
+    <div className="fixed top-0 left-0 h-screen bg-sidebar w-1/6">
+      <div className="overflow-y-auto h-full justify-center">
+        {/* Avatar */}
+        <div className="flex flex-row px-5 py-5 items-center text-txt_white gap-2">
+          <div className="flex w-10 bg-gray-500 h-10 items-center justify-center rounded-full mr-2">
+            P
           </div>
-          <div className="ml-4">
-            <h1 className="text-xl font-bold text-white">Shop's Name</h1>
-            <p className="text-sm text-white">Your Shop Slogan</p>
-          </div>
+          <div>Shop's Name</div>
         </div>
-        <hr className="border-t border-gray-400 my-5" />
-
-        {/* Drawer Items */}
-        {DRAWER_ITEM.map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-between p-5 hover:bg-gray-800 cursor-pointer"
-            onClick={() => navigate(item.link)}
-          >
-            <span className="text-lg text-white">{item.name}</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-white"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 11.293a1 1 0 011.414 0L10 14.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414 1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        ))}
+        <Divider color="bg-hover"/>
+        {DRAWER_ITEM.map((item, index) => {
+          return (
+            <DrawerItem onClick={() => {navigate(item.link); setCurrentTab(item.link)}} name={item.name} icon1={item.icon1} icon2={item.icon2} isSelected={currentTab===item.link}/>
+          );
+        })}
       </div>
     </div>
   );
