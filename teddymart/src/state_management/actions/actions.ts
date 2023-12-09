ce3a534ea6bc452ec3cea3927c83209e3ae7b941
Tien25012003 <21520112@gm.uit.edu.vn>
2023-12-09 20:43:59 +0700

    Feat - Delete Report

--- teddymart/src/state_management/actions/actions.ts
+++ teddymart/src/state_management/actions/actions.ts
@@ -2,4 +2,5 @@ import { createAction } from "@reduxjs/toolkit";
 
 export const RESET_ALL_STORES = createAction("RESET_ALL_STORES");
 export const ADD_ORDER = createAction("ADD_ORDER");
+export const DELETE_ORDER = createAction("DELETE_ORDER");
 export const DELETE_PRODUCT = createAction("DELETE_PRODUCT");
--- teddymart/src/state_management/slices/reportSlice.ts
+++ teddymart/src/state_management/slices/reportSlice.ts
@@ -1,5 +1,9 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { ADD_ORDER, RESET_ALL_STORES } from "state_management/actions/actions";
+import {
+  ADD_ORDER,
+  DELETE_ORDER,
+  RESET_ALL_STORES,
+} from "state_management/actions/actions";
 
 const reportSlice = createSlice({
   name: "reportSlice",
@@ -66,37 +70,94 @@ const reportSlice = createSlice({
         } else {
           if (order.type === "Import" && order.status === "paid") {
             state.byDate[i].outcome += order.totalPayment;
-            state.byMonth[i].outcome += order.totalPayment;
-            state.byYear[i].outcome += order.totalPayment;
+            state.byMonth[iM].outcome += order.totalPayment;
+            state.byYear[iY].outcome += order.totalPayment;
 
             state.byDate[i].profit -= order.totalPayment;
-            state.byMonth[i].profit -= order.totalPayment;
-            state.byYear[i].profit -= order.totalPayment;
+            state.byMonth[iM].profit -= order.totalPayment;
+            state.byYear[iY].profit -= order.totalPayment;
           }
           if (order.type === "Export" && order.status === "paid") {
             state.byDate[i].revenue += order.totalPayment;
-            state.byMonth[i].revenue += order.totalPayment;
-            state.byYear[i].revenue += order.totalPayment;
+            state.byMonth[iM].revenue += order.totalPayment;
+            state.byYear[iY].revenue += order.totalPayment;
 
             state.byDate[i].profit += order.totalPayment;
-            state.byMonth[i].profit += order.totalPayment;
-            state.byYear[i].profit += order.totalPayment;
+            state.byMonth[iM].profit += order.totalPayment;
+            state.byYear[iY].profit += order.totalPayment;
           }
           if (order.type === "Import") {
             state.byDate[i].importOrder += 1;
-            state.byMonth[i].importOrder += 1;
-            state.byYear[i].importOrder += 1;
+            state.byMonth[iM].importOrder += 1;
+            state.byYear[iY].importOrder += 1;
           } else {
             state.byDate[i].exportOrder += 1;
-            state.byMonth[i].exportOrder += 1;
-            state.byYear[i].exportOrder += 1;
+            state.byMonth[iM].exportOrder += 1;
+            state.byYear[iY].exportOrder += 1;
           }
 
           state.byDate[i].numberOfOrder += 1;
-          state.byMonth[i].numberOfOrder += 1;
-          state.byYear[i].numberOfOrder += 1;
+          state.byMonth[iM].numberOfOrder += 1;
+          state.byYear[iY].numberOfOrder += 1;
         }
 
+        return state;
+      }
+    );
+    builder.addCase(
+      DELETE_ORDER,
+      (state: TReportSlice, action: PayloadAction<TOrder[]>) => {
+        for (const order of action.payload) {
+          //let index = state.byDate.findIndex(())
+          let iD = state.byDate.findIndex(
+            (d) =>
+              new Date(d.date).getTime() === new Date(order.createdAt).getTime()
+          );
+          let iM = state.byMonth.findIndex(
+            (m) =>
+              new Date(m.date).getTime() === new Date(order.createdAt).getTime()
+          );
+          let iY = state.byYear.findIndex(
+            (y) =>
+              new Date(y.date).getTime() === new Date(order.createdAt).getTime()
+          );
+
+          if (iD !== -1) {
+            if (order.type === "Export") {
+              state.byDate[iD].exportOrder -= 1;
+              state.byMonth[iM].exportOrder -= 1;
+              state.byYear[iY].exportOrder -= 1;
+              if (order.status === "paid") {
+                state.byDate[iD].revenue -= order.totalPayment;
+                state.byMonth[iM].revenue -= order.totalPayment;
+                state.byYear[iY].revenue -= order.totalPayment;
+
+                state.byDate[iD].profit -= order.totalPayment;
+                state.byMonth[iM].profit -= order.totalPayment;
+                state.byMonth[iY].profit -= order.totalPayment;
+              }
+            }
+
+            if (order.type === "Import") {
+              state.byDate[iD].importOrder -= 1;
+              state.byMonth[iM].importOrder -= 1;
+              state.byYear[iY].importOrder -= 1;
+              if (order.status === "paid") {
+                state.byDate[iD].outcome += order.totalPayment;
+                state.byMonth[iM].outcome += order.totalPayment;
+                state.byYear[iY].outcome += order.totalPayment;
+
+                state.byDate[iD].profit += order.totalPayment;
+                state.byMonth[iM].profit += order.totalPayment;
+                state.byMonth[iY].profit += order.totalPayment;
+              }
+            }
+
+            state.byDate[iD].numberOfOrder -= 1;
+            state.byMonth[iM].numberOfOrder -= 1;
+            state.byYear[iY].numberOfOrder -= 1;
+          }
+        }
         return state;
       }
     );
