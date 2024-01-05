69c45b1ee0947674c0e4878c34c3338507d1f81d
Tien25012003 <21520112@gm.uit.edu.vn>
2024-01-05 22:58:30 +0700

    Use redux persist

--- teddymart/src/App.js
+++ teddymart/src/App.js
@@ -1,14 +1,21 @@
 import RootRoutes from "./routes/route/RootRoutes";
-import { store } from "state_management/stores/store";
+import { store, persistor } from "state_management/stores/store";
 import { Provider } from "react-redux";
 import { Notifications } from "react-push-notification";
 import Draft from "./views/Draft/Draft";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { useHistory } from "react-router-dom";
 const App = () => {
   return (
     <Provider store={store}>
+      {/* <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+        <>
+          <RootRoutes />
+          <Notifications />
+        </>
+      </PersistGate> */}
       <RootRoutes />
       <Notifications />
-      {/* <Draft /> */}
     </Provider>
   );
 };
--- teddymart/src/state_management/stores/store.ts
+++ teddymart/src/state_management/stores/store.ts
@@ -1,12 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "state_management/reducers/rootReducer";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: pReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
   // devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 });
+export const persistor = persistStore(store);
